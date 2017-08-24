ba7650d237e93b42d9dc6f7a6870b787c9e4cc0f
Neilson Gamble <neilgamb@Neilsons-MacBook-Pro.local>
2017-08-23 21:50:09 -0400

    detail boxes added with animation

--- app.js
+++ app.js
@@ -51,162 +51,180 @@ window.addEventListener('load', function () {
 
     // Triple C
     const lenny = [-80.8735, 35.2013]
-    
-        const lennyEl = document.createElement('div');
-        lennyEl.classList.add('marker')
-        lennyEl.id = 'lennyMark';
-    
-        new mapboxgl.Marker(lennyEl, { offset: [-25, -25] })
-            .setLngLat(lenny)
-            .addTo(map);
+
+    const lennyEl = document.createElement('div');
+    lennyEl.classList.add('marker')
+    lennyEl.id = 'lennyMark';
+
+    new mapboxgl.Marker(lennyEl, { offset: [-25, -25] })
+        .setLngLat(lenny)
+        .addTo(map);
 
     // Sycamore
     const syc = [-80.862762, 35.208713]
-    
-        const sycEl = document.createElement('div');
-        sycEl.classList.add('marker')
-        sycEl.id = 'sycMark';
-    
-        new mapboxgl.Marker(sycEl, { offset: [-25, -25] })
-            .setLngLat(syc)
-            .addTo(map);
+
+    const sycEl = document.createElement('div');
+    sycEl.classList.add('marker')
+    sycEl.id = 'sycMark';
+
+    new mapboxgl.Marker(sycEl, { offset: [-25, -25] })
+        .setLngLat(syc)
+        .addTo(map);
 
     // Wooden Robot
     const wb = [-80.857001, 35.216899]
-    
-        const wbEl = document.createElement('div');
-        wbEl.classList.add('marker')
-        wbEl.id = 'wbMark';
-    
-        new mapboxgl.Marker(wbEl, { offset: [-25, -25] })
-            .setLngLat(wb)
-            .addTo(map);
+
+    const wbEl = document.createElement('div');
+    wbEl.classList.add('marker')
+    wbEl.id = 'wbMark';
+
+    new mapboxgl.Marker(wbEl, { offset: [-25, -25] })
+        .setLngLat(wb)
+        .addTo(map);
 
     // Unknown
     const unk = [-80.857522, 35.220503]
-    
-        const unkEl = document.createElement('div');
-        unkEl.classList.add('marker')
-        unkEl.id = 'unkMark';
-    
-        new mapboxgl.Marker(unkEl, { offset: [-25, -25] })
-            .setLngLat(unk)
-            .addTo(map);
+
+    const unkEl = document.createElement('div');
+    unkEl.classList.add('marker')
+    unkEl.id = 'unkMark';
+
+    new mapboxgl.Marker(unkEl, { offset: [-25, -25] })
+        .setLngLat(unk)
+        .addTo(map);
 
     // Legion
     const leg = [-80.812991, 35.218373]
-    
-        const legEl = document.createElement('div');
-        legEl.classList.add('marker')
-        legEl.id = 'legMark';
-    
-        new mapboxgl.Marker(legEl, { offset: [-25, -25] })
-            .setLngLat(leg)
-            .addTo(map);
+
+    const legEl = document.createElement('div');
+    legEl.classList.add('marker')
+    legEl.id = 'legMark';
+
+    new mapboxgl.Marker(legEl, { offset: [-25, -25] })
+        .setLngLat(leg)
+        .addTo(map);
 
     // Birdsong
     const bird = [-80.826533, 35.230651]
-    
-        const birdEl = document.createElement('div');
-        birdEl.classList.add('marker')
-        birdEl.id = 'birdMark';
-    
-        new mapboxgl.Marker(birdEl, { offset: [-25, -25] })
-            .setLngLat(bird)
-            .addTo(map);
+
+    const birdEl = document.createElement('div');
+    birdEl.classList.add('marker')
+    birdEl.id = 'birdMark';
+
+    new mapboxgl.Marker(birdEl, { offset: [-25, -25] })
+        .setLngLat(bird)
+        .addTo(map);
 
     // NoDa
     const noda = [-80.812471, 35.251562]
-    
-        const nodaEl = document.createElement('div');
-        nodaEl.classList.add('marker')
-        nodaEl.id = 'nodaMark';
-    
-        new mapboxgl.Marker(nodaEl, { offset: [-25, -25] })
-            .setLngLat(noda)
-            .addTo(map);
+
+    const nodaEl = document.createElement('div');
+    nodaEl.classList.add('marker')
+    nodaEl.id = 'nodaMark';
+
+    new mapboxgl.Marker(nodaEl, { offset: [-25, -25] })
+        .setLngLat(noda)
+        .addTo(map);
 
     // Free Range
     const fr = [-80.813282, 35.240505]
-    
-        const frEl = document.createElement('div');
-        frEl.classList.add('marker')
-        frEl.id = 'frMark';
-    
-        new mapboxgl.Marker(frEl, { offset: [-25, -25] })
-            .setLngLat(fr)
-            .addTo(map);
+
+    const frEl = document.createElement('div');
+    frEl.classList.add('marker')
+    frEl.id = 'frMark';
+
+    new mapboxgl.Marker(frEl, { offset: [-25, -25] })
+        .setLngLat(fr)
+        .addTo(map);
 
     // Thirsty Nomad
     const tn = [-80.889842, 35.188583]
-    
-        const tnEl = document.createElement('div');
-        tnEl.classList.add('marker')
-        tnEl.id = 'tnMark';
-    
-        new mapboxgl.Marker(tnEl, { offset: [-25, -25] })
-            .setLngLat(tn)
-            .addTo(map);
+
+    const tnEl = document.createElement('div');
+    tnEl.classList.add('marker')
+    tnEl.id = 'tnMark';
+
+    new mapboxgl.Marker(tnEl, { offset: [-25, -25] })
+        .setLngLat(tn)
+        .addTo(map);
 
     // Blue Blaze
     const blubl = [-80.867545, 35.240870]
-    
-        const blublEl = document.createElement('div');
-        blublEl.classList.add('marker')
-        blublEl.id = 'blublMark';
-    
-        new mapboxgl.Marker(blublEl, { offset: [-25, -25] })
-            .setLngLat(blubl)
-            .addTo(map);
+
+    const blublEl = document.createElement('div');
+    blublEl.classList.add('marker')
+    blublEl.id = 'blublMark';
+
+    new mapboxgl.Marker(blublEl, { offset: [-25, -25] })
+        .setLngLat(blubl)
+        .addTo(map);
 
     // Heist
     const heist = [-80.809440, 35.245478]
-    
-        const heistEl = document.createElement('div');
-        heistEl.classList.add('marker')
-        heistEl.id = 'heistMark';
-    
-        new mapboxgl.Marker(heistEl, { offset: [-25, -25] })
-            .setLngLat(heist)
-            .addTo(map);
+
+    const heistEl = document.createElement('div');
+    heistEl.classList.add('marker')
+    heistEl.id = 'heistMark';
+
+    new mapboxgl.Marker(heistEl, { offset: [-25, -25] })
+        .setLngLat(heist)
+        .addTo(map);
 
     // Resident Culture
     const rescul = [-80.805504, 35.220206]
-    
-        const resculEl = document.createElement('div');
-        resculEl.classList.add('marker')
-        resculEl.id = 'resculMark';
-    
-        new mapboxgl.Marker(resculEl, { offset: [-25, -25] })
-            .setLngLat(rescul)
-            .addTo(map);
+
+    const resculEl = document.createElement('div');
+    resculEl.classList.add('marker')
+    resculEl.id = 'resculMark';
+
+    new mapboxgl.Marker(resculEl, { offset: [-25, -25] })
+        .setLngLat(rescul)
+        .addTo(map);
 
     // Salud
     const salud = [-80.804100, 35.247787]
-    
-        const saludEl = document.createElement('div');
-        saludEl.classList.add('marker')
-        saludEl.id = 'saludMark';
-    
-        new mapboxgl.Marker(saludEl, { offset: [-25, -25] })
-            .setLngLat(salud)
-            .addTo(map);
+
+    const saludEl = document.createElement('div');
+    saludEl.classList.add('marker')
+    saludEl.id = 'saludMark';
+
+    new mapboxgl.Marker(saludEl, { offset: [-25, -25] })
+        .setLngLat(salud)
+        .addTo(map);
+
+
+
 })
 
 function makeBig(x) {
 
     let id = x.id + "Mark"
-    console.log(typeof (id))
-    document.getElementById(id).style.height = "100px";
-    document.getElementById(id).style.width = "100px";
-
+    document.getElementById(id).style.height = "150px";
+    document.getElementById(id).style.width = "150px";
+    document.getElementById(id).style.transition = "1s"
 }
 
 function makeNorm(x) {
 
     let id = x.id + "Mark"
-    console.log(typeof (id))
     document.getElementById(id).style.height = "30px";
     document.getElementById(id).style.width = "30px";
-
+    document.getElementById(id).style.transition = "none"
+    
 }
+
+function openDetail(x){
+    let id = x.id + "Detail";
+    let details = document.getElementsByClassName('detail');
+    
+    for(let i = 0; i < details.length; i++){
+        details[i].classList.remove('active');
+    }
+
+    document.getElementById(id).classList.add('active');
+}
+
+// function closeDetail(x){
+//     let id = x.id + "Detail";
+//     document.getElementById(id).classList.remove('active');  
+// }
